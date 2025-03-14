526a833c3cd7a8ebb5f6850315aef68fe3b8ad77
R-azy <muhammedrazy1798@gmail.com>
2025-03-14 09:39:40 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 // src/App.jsx
 
-import React from 'react';
-import Home from './Home';
-import './App.css'; // Import global styles
+import React from "react";
+import Home from "./Home";
+import { Form } from "./Form";
+import "./App.css"; // Import global styles
+import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/addbook" element={<Form />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
